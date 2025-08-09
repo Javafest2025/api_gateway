59843d238f5e35fe004ca9151b245807452a2b17
Tasriad Ahmed Tias <trisn.eclipse@gmail.com>
2025-08-09 14:03:12 +0600

    CORS issuses fixed.

--- src/main/java/org/solace/scholar_ai/api_gateway/ApiGatewayApplication.java
+++ src/main/java/org/solace/scholar_ai/api_gateway/ApiGatewayApplication.java
@@ -2,10 +2,8 @@ package org.solace.scholar_ai.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@CrossOrigin
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
